1af158f955aec226578fa458c9e54774efcb7bb8
Frimousse <beryl.beedham@gmail.com>
2025-02-26 15:22:29 +0100

    Code simplification database

--- baseDDonnees_csv/detectionconstellation.py
+++ baseDDonnees_csv/detectionconstellation.py
@@ -8,19 +8,19 @@ message = [line.strip() for line in message if line]
 # Change ce dico avec les coordonnées correctes que tu veux retrouver (x,y)
 correct_dist = [(388,586), (126.67, 877.08), (427.6, 1000), (478.6, 433.68), (1056, 277), (532, 58.65)]
 
-distances = [ [] for _ in range(len(correct_dist)) ]
+distances = {i: [] for i in range(len(correct_dist))}
 
 for line in message:
     x, y, light = line.split(",")
     x = float(x)
     y = float(y)
-    for  i in range(len(correct_dist)):
+    for i in range(len(correct_dist)):
         dist = abs(x - correct_dist[i][0])**2 + abs(y - correct_dist[i][1])**2
-        distances[i].append(dist)
+        distances[i].append((dist, line))
 
 most_probable_guesses = []
 for i in range(len(distances)):
-    min_key = distances[i].index(min(distances[i]))
-    most_probable_guesses.append(message[min_key])
+    min_dist, min_line = min(distances[i], key=lambda t: t[0])
+    most_probable_guesses.append(min_line)
 
 print(most_probable_guesses)
