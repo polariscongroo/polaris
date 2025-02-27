eec2b6c0a53400bbaea8d1649b27392ae16e8cee
BuildTools <emmamell@free.fr>
2025-02-27 18:24:42 +0100

    Création d'autre tests (vides pour l'instant)

---
+++ recognition/src/tsp/polaris/JUnitTest/List_PointTest.java
@@ -0,0 +1,13 @@
+package tsp.polaris.JUnitTest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class List_PointTest {
+    
+    @Test
+    @DisplayName("Test 1 : TODO")
+    public void test() {
+        // TODO
+    }
+}
