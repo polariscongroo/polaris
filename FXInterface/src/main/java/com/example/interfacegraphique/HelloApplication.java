2f2569042967756d3a9be6841265f0b2d9804d36
Frimousse <beryl.beedham@gmail.com>
2025-04-11 10:21:40 +0200

    Interface Graphique

--- FXInterface/src/main/java/com/example/interfacegraphique/HelloApplication.java
+++ FXInterface/src/main/java/com/example/interfacegraphique/HelloApplication.java
@@ -74,3 +74,35 @@ public class HelloApplication extends Application {
         launch(args);
     }
 }
+
+
+/*package com.example.interfacegraphique;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Polaris");
+        stage.setScene(scene);
+        stage.setResizable(true);
+        stage.sizeToScene();
+
+        stage.show();
+
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+} */
