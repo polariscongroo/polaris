0681dd183d547cfd35e1ce901927bbbd3338409a
chadi_aat <chadiaitek2004@gmail.com>
2025-04-14 14:56:26 +0200

    Revert "clean repo"
    This reverts commit 58d9e96607452aaf64200e41c64a69d13de4fe84.

---
+++ FXInterface/src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module com.example.interfacegraphique {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires javafx.media;
+    requires javafx.graphics;
+    requires java.desktop;
+
+    opens com.example.interfacegraphique to javafx.fxml;
+    exports com.example.interfacegraphique;
+}
+
