b107ac090b248978998f2d0fb03c728b4e1f73bb
Ry-Se-afk <sryane046@gmail.com>
2025-04-26 21:52:21 +0200

    proposition vidéo nouveau style

--- FXInterface/src/main/java/com/example/interfacegraphique/HelloController.java
+++ FXInterface/src/main/java/com/example/interfacegraphique/HelloController.java
@@ -157,7 +157,7 @@ public void eraser(String path) {
 
 private void setupBackgroundVideo() {
     try {
-        URL videoUrl = getClass().getResource("/images/propRyane.mp4");
+        URL videoUrl = getClass().getResource("/images/test2.mp4");
         if (videoUrl == null) {
             System.err.println("ERREUR: Fichier vidéo introuvable dans /images/propEmma.mp4");
             return;
